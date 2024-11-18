733887e62f9288deb81df3831e8dfc75a3a47786
Juan Ignacio Sánchez Sampayo <juan@192.168.1.23>
2024-11-18 00:43:46 -0300

    deploy using astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://juan-ignacio-sanchez.github.io",
+    base: "portfolio2"
+});
